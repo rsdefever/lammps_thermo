94c1588c85903b98d48a6fe069f61bc817b5ed01
Ryan DeFever <rdefever@nd.edu>
2019-11-15 08:13:21 -0500

    Initial cookiecutter cleanup

--- lammps_thermo/lammps_thermo.py
+++ lammps_thermo/lammps_thermo.py
@@ -1,34 +1,104 @@
-"""
-lammps_thermo.py
-Lightweight class to extract and manipulate thermo info from lammps log files
+# Ryan DeFever
+# Maginn Research Group
+# University of Notre Dame
+# 2019 Nov 15
 
-Handles the primary functions
-"""
-
-
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
+class LAMMPSThermo:
+    """Extract and manipulate thermodynamic information
+    from a LAMMPS log file
     """
 
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
+    def __init__(self,filename,start_keyword='Step',end_keyword='Loop',
+            skip_sections=0):
+        """Create the LAMMPSThermo object
 
+        Parameters
+        ----------
+        filename : string 
+            Name of the LAMMPS logfile
+        start_keyword  : string, optional
+            First word on the header line before the
+            thermo data of interest
+        end_keyword : string, optional
+            First word on the line following the
+            thermo data of interest
+        skip_sections : int, optional
+            Number of sections of thermo data
+            to skip before reading in data
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+        Returns:
+        --------
+        thermo_data : LAMMPSThermo
+            Structure with thermo data extracted
+            from lammps log file
+
+        """
+
+        self.header_map, self.data = _read_thermo_data(filename,
+                start_keyword,end_keyword,skip_sections)
+
+    def _read_thermo_data(filename,start_keyword,end_keyword,
+            skip_sections):
+         """Reads filename and extracts thermo information
+
+        Parameters
+        ----------
+        filename : string 
+            Name of the LAMMPS logfile
+        start_keyword : string
+            First word on the header line before the
+            thermo data of interest
+        end_keyword : string, optional
+            First word on the line following the
+            thermo data of interest
+
+        Returns:
+        --------
+        extracted_data : np.ndarray
+        col_map : dict
+
+        """
+        log_data = []
+        with open(filename) as log_file:
+            for line in log_file:
+                log_data.append(line.strip().split())
+
+        # Extract the relevant thermo data
+        found_sections = 0
+        for i,line in enumerate(log_data):
+            try:
+                if line[0] == start_keyword:
+                    start_idx = i
+                    found_sections += 1
+                if line[0] == end_keyword:
+                    end_idx = i
+                    if found_sections > skip_sections:
+                        break
+            except IndexError:
+                continue
+
+        # Make sure we found starting and ending indices
+        try:
+            start_idx
+        except NameError:
+            raise NameError('Keyword {} not found in LAMMPS log file'.format(
+                keyword))
+        try:
+            end_idx
+        except NameError:
+            raise NameError('Ending keyword {} not found in'
+                    'LAMMPS log file'.format(end_keyword))
+
+        n_lines = end_idx - start_idx
+        assert n_lines > 0
+        
+        # Extract column headers and create dictionary to map
+        # thermo property name to column index
+        col_headers = data[start_idx]
+        col_map = { prop : i for i,prop in enumerate(col_headers) }
+
+        # Extract data into numpy array
+        extracted_data = np.loadtxt(filen,skiprows=start_idx,max_rows=n_lines)
+
+        return col_map, extracted_data
+
