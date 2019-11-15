5b0c640c67d1abc23e07099209a66246960b2a95
Ryan DeFever <rdefever@nd.edu>
2019-11-15 09:05:31 -0500

    Intermediate edits

--- lammps_thermo/lammps_thermo.py
+++ lammps_thermo/lammps_thermo.py
@@ -3,6 +3,8 @@
 # University of Notre Dame
 # 2019 Nov 15
 
+import numpy as np
+
 class LAMMPSThermo:
     """Extract and manipulate thermodynamic information
     from a LAMMPS log file
@@ -34,17 +36,37 @@ class LAMMPSThermo:
 
         """
 
-        self.header_map, self.data = _read_thermo_data(filename,
+        self.filename = filename
+
+        self.header_map, self.data = self._read_thermo_data(
                 start_keyword,end_keyword,skip_sections)
 
-    def _read_thermo_data(filename,start_keyword,end_keyword,
-            skip_sections):
-         """Reads filename and extracts thermo information
+    def extract_property(self,prop):
+        """Extracts the desired property
 
         Parameters
         ----------
-        filename : string 
-            Name of the LAMMPS logfile
+        prop : string
+            Name of thermo property to extract. This should match
+            the keywords used in the lammps 'thermo_style' command.
+
+        Returns:
+        --------
+        property_data : np.ndarray
+
+        """
+
+        if prop not in self.header_map.keys():
+            raise ValueError("Selected property does not exist in the"
+                    "LAMMPSThermo object. Available choices are: "
+                    "{}".format(self.header_map.keys()))
+
+    def _read_thermo_data(self,start_keyword,end_keyword,
+            skip_sections):
+        """Reads filename and extracts thermo information
+
+        Parameters
+        ----------
         start_keyword : string
             First word on the header line before the
             thermo data of interest
@@ -58,9 +80,10 @@ class LAMMPSThermo:
         col_map : dict
 
         """
+
         log_data = []
-        with open(filename) as log_file:
-            for line in log_file:
+        with open(self.filename) as log_file:
+            for i,line in enumerate(log_file):
                 log_data.append(line.strip().split())
 
         # Extract the relevant thermo data
@@ -94,11 +117,12 @@ class LAMMPSThermo:
         
         # Extract column headers and create dictionary to map
         # thermo property name to column index
-        col_headers = data[start_idx]
+        col_headers = log_data[start_idx]
         col_map = { prop : i for i,prop in enumerate(col_headers) }
 
         # Extract data into numpy array
-        extracted_data = np.loadtxt(filen,skiprows=start_idx,max_rows=n_lines)
+        extracted_data = np.loadtxt(self.filename,skiprows=start_idx+1,max_rows=n_lines-1)
 
         return col_map, extracted_data
+
 
